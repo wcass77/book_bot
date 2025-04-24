bd2813c39a6e19a83388b0cb47b619f8d3317a30
Willy Cass <1844021+wcass77@users.noreply.github.com>
2025-04-24 10:48:08 -0400

    refactor: add type hints to function signatures in main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-FRANK_PATH = "books/frankenstein.txt"
+FRANK_PATH: str = "books/frankenstein.txt"
 
 
-def get_book_text(file_path):
+def get_book_text(file_path: str) -> str:
     with open(file_path) as f:
         text = f.read()
     return text
 
 
-def main():
-    print(get_book_text(FRANK_PATH))
+def word_count(text: str) -> int:
+    return len(text.split())
+
+
+def main() -> None:
+    text = get_book_text(FRANK_PATH)
+    wc = word_count(text)
+    print(f"{wc} words found in the document")
 
 
 main()
