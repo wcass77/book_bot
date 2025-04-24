d5989dcdd17ae8291e28c3764b11ac5af0cbf045
Willy Cass <1844021+wcass77@users.noreply.github.com>
2025-04-24 10:50:28 -0400

    feat: implement word_count function in stats.py and update main.py to use it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 FRANK_PATH: str = "books/frankenstein.txt"
 
 
@@ -5,10 +7,6 @@ def get_book_text(file_path: str) -> str:
     with open(file_path) as f:
         text = f.read()
     return text
-
-
-def word_count(text: str) -> int:
-    return len(text.split())
 
 
 def main() -> None:
---
+++ stats.py
@@ -0,0 +1,2 @@
+def word_count(text: str) -> int:
+    return len(text.split())
